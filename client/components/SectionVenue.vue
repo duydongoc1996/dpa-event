42c368df68cb0797bf3f24e9c8038f3d5fef3f74
duydongoc1996@gmail.com <duy.do@fdssoft.com>
2019-07-17 11:50:13 +0700

    add google map

--- client/components/SectionVenue.vue
+++ client/components/SectionVenue.vue
@@ -15,14 +15,18 @@
           b-col(
             sm="8"
           )
-            div.ven-map
+            div.ven-map(
+              v-html="gmap"
+            )
 </template>
 
 <script>
 export default {
   name: 'SectionVenue',
   data() {
-    return {}
+    return {
+      gmap: '<div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="100%" id="gmap_canvas" src="https://maps.google.com/maps?q=640%2C%20Eonuj-ro%20Nonhyeon%202(1)-Dong%2C%20Gangnam-gu%2C%20Seoul&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.enable-javascript.net">enable-javascript.net</a></div><style>.mapouter{position:relative;text-align:right;height:100%;width:100%;}.gmap_canvas {overflow:hidden;background:none!important;height:100%;width:100%;}</style></div>'
+    }
   }
 }
 </script>
