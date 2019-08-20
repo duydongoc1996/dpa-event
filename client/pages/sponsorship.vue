3d8d2261278fdaa1f35bdb3a853a2935bb205e68
shira <duydongoc1996@gmail.com>
2019-08-20 10:31:31 +0700

    add to staging

--- client/pages/sponsorship.vue
+++ client/pages/sponsorship.vue
@@ -4,29 +4,29 @@
       Menu
     section.section-header
       SectionHeader
-    //- div(
-    //-   v-if="!showSubmit"
-    //- )
-    //-   section.section-info
-    //-     SectionInfo
-    //-   section.section-price-table(
-    //-     v-if="!showSelection"
-    //-   )
-    //-     SectionPriceTable(
-    //-       :checked.sync="sponsorType"
-    //-       @update:checked="changeView"
-    //-     )
-    //-   section.section-sponsorship-form(
-    //-     v-if="showSelection"
-    //-   )
-    //-     SectionSponsorshipForm(
-    //-       @update:submitted="changeSubmitView"
-    //-       :sponsorshipType="sponsorType"
-    //-     )
-    //- section.section-submit-success(
-    //-   v-if="showSubmit"
-    //- )
-    //-   SectionSubmitSuccess
+    div(
+      v-if="!showSubmit"
+    )
+      section.section-info
+        SectionInfo
+      section.section-price-table(
+        v-if="!showSelection"
+      )
+        SectionPriceTable(
+          :checked.sync="sponsorType"
+          @update:checked="changeView"
+        )
+      section.section-sponsorship-form(
+        v-if="showSelection"
+      )
+        SectionSponsorshipForm(
+          @update:submitted="changeSubmitView"
+          :sponsorshipType="sponsorType"
+        )
+    section.section-submit-success(
+      v-if="showSubmit"
+    )
+      SectionSubmitSuccess
     section.section-footer
       SectionFooter
 </template>
